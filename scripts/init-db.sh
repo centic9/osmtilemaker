194b8cb041495b41358dc367f5bd15572855e579
Dominik Stadler <dominik.stadler@gmx.at>
2023-04-23 20:41:19 +0200

    Change locale for postgres to de_AT.utf8
    en_US.utf8 may not be installed
    also requires template0

--- scripts/init-db.sh
+++ scripts/init-db.sh
@@ -15,15 +15,16 @@ echo Create PGSQL user and database with extensions
 echo
 
 psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" <<-EOSQL
-    CREATE USER $DBPG_USER_OSMTILEMAKER_USERNAME 
+    CREATE USER $DBPG_USER_OSMTILEMAKER_USERNAME
         NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;
     CREATE DATABASE $DBPG_DATABASE_NAME
         WITH OWNER = $DBPG_USER_OSMTILEMAKER_USERNAME
             ENCODING = 'UTF8'
             TABLESPACE = pg_default
-            LC_COLLATE = 'en_US.utf8'
-            LC_CTYPE = 'en_US.utf8'
+            LC_COLLATE = 'de_AT.utf8'
+            LC_CTYPE = 'de_AT.utf8'
             CONNECTION LIMIT = -1
+            TEMPLATE template0
 EOSQL
 
 psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d "$DBPG_DATABASE_NAME" <<-EOSQL
